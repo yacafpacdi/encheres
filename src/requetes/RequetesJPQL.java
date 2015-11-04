4a06f569a5c4646c8d0a192fca8bcc84fa44a8fd
ybcdinfo <yb.cdinfo@gmail.com>
2015-11-04 07:43:11 +0100

    ajout de quelques requetes jpql

--- src/requetes/RequetesJPQL.java
+++ src/requetes/RequetesJPQL.java
@@ -1,5 +1,6 @@
 package requetes;
 
+import entites.Produit;
 import entites.Vente;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -11,21 +12,32 @@ public class RequetesJPQL {
     
     public static void main(String[] args) {
         System.out.println("=================DEBUT=======================");
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("encheresMySQLPU");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("encheresPU");
         EntityManager em = emf.createEntityManager();
         
         Outils<Vente> ov = new Outils<>();
+        Outils<Produit> op = new Outils<>();
         
-        System.out.println("1) Afficher toutes ventes dans une catégorie.");
-        String req = "";
+        Long id=3L;
+        System.out.println("1) Afficher toutes ventes dans la catégorie: "+id);
+        String req = "select p.ventes from Produit p where p.categorie.id=:idParam";
         Query qr = em.createQuery(req);
+        qr.setParameter("idParam", id);
         List<Vente> lv = qr.getResultList();
         ov.afficher(lv);
-        System.out.println("2) Afficher une liste de produits d'une catégorie.");
-        System.out.println("3) Afficher les catégories de vente d'un utilisateur.");
-        System.out.println("4) Afficher les produits qui ont des enchères.");
-        System.out.println("5) Afficher les produits qui ont été vendus.");
-        System.out.println("6) Afficher les ventes d'une catégorie par un utilisateur.");
+        System.out.println("2) Afficher tous les produits dans la catégorie: "+id);
+        req = "select c.produits from Categorie c where c.id=:idParam"; 
+//      req = "select p from Produit p where p.categorie.id=:idParam";
+        qr=em.createQuery(req);
+        qr.setParameter("idParam", id);
+        List<Produit> lp = qr.getResultList();
+        op.afficher(lp);                
+        System.out.println("3) Afficher les catégories de vente de l'utilisateur: "+id);
+        req ="select distinct p.categorie from Produit p " ;
+        
+//        System.out.println("4) Afficher les produits qui ont des enchères.");
+//        System.out.println("5) Afficher les produits qui ont été vendus.");
+//        System.out.println("6) Afficher les ventes d'une catégorie par un utilisateur.");
         
         
         System.out.println("=================FIN=======================");
